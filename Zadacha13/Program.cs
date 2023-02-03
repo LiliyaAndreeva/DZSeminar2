e854dfa5bd7380c642c67fd67afff8c265ba44ca
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-02-03 16:10:52 +0500

    Добавила задачу №13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая показывает 3
+// цифру числа и сообщается если 3й цифры нет
+
+
+Console.Clear();
+Console.WriteLine("Введите число");  
+string num = Convert.ToString (Console.ReadLine());  
+int l = num.Length; 
+if (l < 3)  
+{  
+Console.WriteLine ("у числа нет третьей цифры");   
+ }  
+else {  
+string num3 = Convert.ToString(num[2]);  
+Console.WriteLine ($"Третья цифра от {num} - > {num3}");  
+}
